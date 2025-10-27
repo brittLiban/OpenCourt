cb4967788a97cac2c79e82a850185962c0ecc2d2
Tia Marie Gordon <tmgordon8192@gmail.com>
2025-10-27 14:05:42 -0700

    Removed production lines that was causing undefined errors on the frontend when running with docker.

--- src/frontend/opencourt/src/api/Games.js
+++ src/frontend/opencourt/src/api/Games.js
@@ -1,11 +1,11 @@
 export async function getGames() {
-  const res = await fetch(`http://${import.meta.env.VITE_SERVER_HOST}:${import.meta.env.VITE_SERVER_PORT}/games`);
+  const res = await fetch(`/games`);
   if (!res.ok) throw new Error("Failed to fetch games");
   return res.json();
 }
 
 export async function createGame(gameData) {
-  const res = await fetch(`http://${import.meta.env.VITE_SERVER_HOST}:${import.meta.env.VITE_SERVER_PORT}/games`, {
+  const res = await fetch(`/games`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(gameData),
