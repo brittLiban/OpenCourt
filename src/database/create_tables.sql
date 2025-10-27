35e034408148ca1b277514688e659ba954ab70a0
Tia Marie Gordon <tmgordon8192@gmail.com>
2025-10-27 15:31:59 -0700

    Fixed typos that was causing issues with creating the tables

--- src/database/create_tables.sql
+++ src/database/create_tables.sql
@@ -3,10 +3,12 @@ DROP TABLE IF EXISTS games;
 DROP TABLE IF EXISTS locations;
 DROP TABLE IF EXISTS users;
 
+
 CREATE TABLE users (
    user_id INT AUTO_INCREMENT PRIMARY KEY,
    user_name VARCHAR(100)
-)
+);
+
 
 CREATE TABLE locations (
    location_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -14,17 +16,19 @@ CREATE TABLE locations (
    address VARCHAR(255)
 );
 
+
 CREATE TABLE games (
    game_id INT AUTO_INCREMENT PRIMARY KEY,
    game_name VARCHAR(100),
-   location_id INT,=
-   FOREIGN KEY (location_id) REFERENCES locations(location_id)
+   location_id INT,
+   FOREIGN KEY (location_id) REFERENCES locations (location_id)
 );
+
 
 CREATE TABLE games_users (
    game_user_id INT AUTO_INCREMENT PRIMARY KEY,
-   game_id INT,
    user_id INT,
-   FOREIGN KEY (game_id) REFERENCES games(game_id),
-   FOREIGN KEY (user_id) REFERENCES users(user_id)
+   game_id INT,
+   FOREIGN KEY (user_id) REFERENCES users (user_id),
+   FOREIGN KEY (game_id) REFERENCES games (game_id)
 );
