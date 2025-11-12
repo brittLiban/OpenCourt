a3a31df433f93a549b21f6dbe711de4542ecfd15
Kimberly Mageary <kimberlymageary@gmail.com>
2025-11-12 14:16:31 -0800

    Fixed state issues when adding a new location.

--- src/frontend/opencourt/src/pages/LocationsPage.jsx
+++ src/frontend/opencourt/src/pages/LocationsPage.jsx
@@ -1,11 +1,25 @@
+import { useState, useEffect } from "react";
 import Locations from '../components/Locations'
 import LocationsForm from '../components/LocationsForm';
+import { getLocations } from "../api/Locations";
 
 function LocationsPage() {
+    const[locations, setLocations] = useState([]);
+
+      useEffect(() => {
+        getLocations()
+        .then(setLocations)
+        .catch((err) => console.error("Error fetching events:", err));
+    }, []);
+
+    const addLocation = (newLocation) => {
+        setLocations(prevLocations => [...prevLocations, newLocation]);
+    };
+
     return( 
     <>
-        <Locations />
-        <LocationsForm />
+        <Locations locations={locations} />
+        <LocationsForm onAddLocation={addLocation} />
     </>
     );
 }
